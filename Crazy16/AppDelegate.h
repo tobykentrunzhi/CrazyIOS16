fccf2980a71cec131d5ba9ab9684af8ef66dbaef
tobykentrunzhi <396367738@qq.com>
2015-11-04 09:55:27 +0800

    Initial Commit

---
+++ Crazy16/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Crazy16
+//
+//  Created by Arron on 11/4/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Crazy16/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Crazy16
+//
+//  Created by Arron on 11/4/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
